dc018b5129d63a48010d3a74afd5cae91c22b71c
gordongecco <delldani001@gmail.com>
2021-09-23 15:29:03 +0200

    ref

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const puppeteer = require("puppeteer");
-let cookies = require("./cookiesJsonfile.json");
 const _ = require("lodash");
 var fs = require("fs");
 
-cookies = cookies.map((item) => {
-  if (item.name === "euconsent-v2") {
-    const newItem = {
-      ...item,
-      value:
-        "CPM707MPM707MAKAiAHUBsCsAP_AAH_AAA6gINtf_X__bX9j-_59f_t0eY1P9_r_v-Qzjhfdt-8F2L_W_L0X42E7NF36pq4KuR4Eu3LBIQNlHMHUTUmwaokVrzHsak2MryNKJ7LEmnMZO2dYGHtPn91TuZKY7_78__fz3z-v_t_-39T37-3_3__5_X---_e_V399zLv9____39nN___9v4INgEmGpeQBdmWODJtGlUKIEYVhIdAKACigGFomsAGBwU7KwCPUELABCagIwIgQYgowYBAAIJAEhEQEgBYIBEARAIAAQAqQEIACJgEFgBYGAQACgGhYgRQBCBIQZHBUcpgQFSLRQS2VgCUXexphCGW-BFAo_oqMBGs0QLAyEhYOY4AkBLxYAAAA.YAAAAAAAAAAA.1.QjY0v9UMqYSBhGAhdjRltA==",
-    };
-    return newItem;
-  } else {
-    return item;
-  }
-});
+// cookies = cookies.map((item) => {
+//   if (item.name === "euconsent-v2") {
+//     const newItem = {
+//       ...item,
+//       value:
+//         "CPM707MPM707MAKAiAHUBsCsAP_AAH_AAA6gINtf_X__bX9j-_59f_t0eY1P9_r_v-Qzjhfdt-8F2L_W_L0X42E7NF36pq4KuR4Eu3LBIQNlHMHUTUmwaokVrzHsak2MryNKJ7LEmnMZO2dYGHtPn91TuZKY7_78__fz3z-v_t_-39T37-3_3__5_X---_e_V399zLv9____39nN___9v4INgEmGpeQBdmWODJtGlUKIEYVhIdAKACigGFomsAGBwU7KwCPUELABCagIwIgQYgowYBAAIJAEhEQEgBYIBEARAIAAQAqQEIACJgEFgBYGAQACgGhYgRQBCBIQZHBUcpgQFSLRQS2VgCUXexphCGW-BFAo_oqMBGs0QLAyEhYOY4AkBLxYAAAA.YAAAAAAAAAAA.1.QjY0v9UMqYSBhGAhdjRltA==",
+//     };
+//     return newItem;
+//   } else {
+//     return item;
+//   }
+// });
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
